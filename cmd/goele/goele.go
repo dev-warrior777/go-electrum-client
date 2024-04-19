15c5f6c3a5dc826a15196c8e502f9f8191374cfe
dev <xorzero777@gmail.com>
2024-04-19 18:24:05 +0800

    app: remove some printlns

--- cmd/goele/goele.go
+++ cmd/goele/goele.go
@@ -193,9 +193,6 @@ func main() {
 	feeRateSatsPerKB, _ := ec.FeeRate(clientCtx, 1)
 	fmt.Println("Sats/KB:", feeRateSatsPerKB)
 
-	// addr, _ := ec.UnusedAddress(clientCtx)
-	// fmt.Println("new address:", addr)
-
 	// for testing only
 	err = btc.RPCServe(ec)
 	if err != nil {
